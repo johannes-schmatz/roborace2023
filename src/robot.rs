354a43d2f64e108b099657a33f15f22cac40f478
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-10-19 14:28:26 +0200

    Make use of the newly added motor abstractions, add more robot states
    these now include state for driving with both the gradient and line follower, as well as a measure state for both of them
    
    we now also have a state switch table, running some methods on state switches, this is for starting up motors for example

--- src/robot.rs
+++ src/robot.rs
@@ -1,7 +1,11 @@
 use anyhow::{Context, Result};
 use ev3dev_lang_rust::Button as Buttons;
 use ev3dev_lang_rust::motors::{LargeMotor, MotorPort};
+use ev3dev_lang_rust::sensors::{ColorSensor, GyroSensor, SensorPort};
+use crate::gradient_follower::GradientFollower;
+use crate::line_follower::LineFollower;
 use crate::menu::{Menu, MenuItem};
+use crate::motor::Motor;
 
 #[derive(Debug)]
 pub struct Robot {
@@ -11,8 +15,13 @@ pub struct Robot {
 
 	menu: Menu,
 
-	left: LargeMotor,
-	right: LargeMotor,
+	line_follower: LineFollower,
+	gradient_follower: GradientFollower,
+
+	color: ColorSensor,
+	//gyro: GyroSensor,
+	left: Motor,
+	right: Motor,
 }
 
 impl Robot {
@@ -20,41 +29,91 @@ impl Robot {
 		let buttons = Buttons::new()
 			.context("Failed to get the robot buttons")?;
 
+		let left = LargeMotor::get(MotorPort::OutA)
+			.context("Failed to get the left motor")?;
+		left.set_polarity(LargeMotor::POLARITY_INVERSED)?;
+		let left = Motor::new(left, "left");
+
+		let right = LargeMotor::get(MotorPort::OutB)
+			.context("Failed to get the right motor")?;
+		right.set_polarity(LargeMotor::POLARITY_INVERSED)?;
+		let right = Motor::new(right, "right");
+
+		let color = ColorSensor::get(SensorPort::In1)
+			.context("Failed to get the color sensor")?;
+
+		//let gyro = GyroSensor::get(SensorPort::In2)
+		//	.context("Failed to get the gyro sensor")?;
+
 		Ok(Robot {
 			state: RobotState::InMenu,
 
 			buttons: buttons.clone(),
 
 			menu: Menu::new(buttons, vec![
-				MenuItem::new("Measure", RobotState::Measure),
-				MenuItem::new("Start", RobotState::Driving),
+				MenuItem::new("Line Measure", RobotState::LineMeasure),
+				MenuItem::new("Line Start", RobotState::LineDrive),
+				MenuItem::new("Gradient Measure", RobotState::GradientMeasure),
+				MenuItem::new("Gradient Start", RobotState::GradientDrive),
 			]),
 
-			left: LargeMotor::get(MotorPort::OutA)
-				.context("Failed to get the left motor")?,
-			right: LargeMotor::get(MotorPort::OutB)
-				.context("Failed to get the right motor")?,
+			line_follower: LineFollower::new(color.clone(), left.clone(), right.clone()),
+			gradient_follower: GradientFollower::new(color.clone(), /*gyro.clone(), */left.clone(), right.clone()),
+
+			color, /*gyro, */left, right,
 		})
 	}
 
 	/// Return `Ok(true)` to end the program, `Ok(false)` otherwise.
 	pub fn tick(&mut self) -> Result<bool> {
+		self.buttons.process();
+		if self.buttons.is_left() {
+			std::thread::sleep(Duration::from_millis(400));
+			self.next_state(RobotState::InMenu)?;
+		}
+
 		match self.state {
 			RobotState::Exit => return Ok(true),
 			RobotState::InMenu => {
 				if let Some(new_state) = self.menu.select()? {
-					self.state = new_state;
+					self.next_state(new_state)?;
 				}
 			},
-			RobotState::Measure => {
-				todo!()
+			RobotState::LineMeasure => {
+				todo!();
+				self.next_state(RobotState::InMenu)?;
 			},
-			RobotState::Driving => {
-				todo!()
+			RobotState::LineDrive => todo!(),
+			RobotState::GradientMeasure => {
+				self.gradient_follower.measure()?;
+				self.next_state(RobotState::InMenu)?;
+			},
+			RobotState::GradientDrive => {
+				self.gradient_follower.drive()?;
 			},
 		}
 
 		Ok(false)
+	}
+
+	fn next_state(&mut self, new_state: RobotState) -> Result<()> {
+		match (&self.state, &new_state) {
+			(_, RobotState::LineMeasure) | (_, RobotState::LineDrive) => {
+				todo!()
+			},
+			(_, RobotState::GradientMeasure) => {},
+			(_, RobotState::GradientDrive) => {
+				self.gradient_follower.prepare_drive()
+					.context("Failed to prepare for gradient drive")?
+			},
+			(RobotState::LineDrive, _) | (RobotState::GradientDrive, _) => {
+				self.left.stop()?;
+				self.right.stop()?;
+			}
+			(_, _) => {},
+		}
+		self.state = new_state;
+		Ok(())
 	}
 }
 
@@ -62,6 +121,8 @@ impl Robot {
 pub enum RobotState {
 	Exit,
 	InMenu,
-	Driving,
-	Measure,
+	LineMeasure,
+	LineDrive,
+	GradientMeasure,
+	GradientDrive,
 }
