6ae20c7b9575711da7bd2935113773af33965250
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-10-24 23:59:38 +0200

    Add command line parsing (for one argument)

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,54 @@ mod pid;
 mod motor;
 mod settings;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use std::time::{Duration, Instant};
 use crate::robot::Robot;
 use crate::settings::Settings;
 
+macro_rules! create_arg {
+    (enum $name:ident { $( $field:ident = $field_text:literal $(,)? )*}) => {
+        #[derive(Debug, Clone)]
+        enum $name {
+            $( $field , )*
+        }
+
+        impl $name {
+            fn get() -> Result<Option<$name>> {
+                std::env::args()
+                    .skip(1)
+                    .next()
+                    .map_or(Ok(None), |string| match string.as_str() {
+                        $(
+                            $field_text => Ok(Some($name::$field)),
+                        )*
+                        x => {
+                            let vec = [
+                                $( $field_text , )*
+                            ];
+                            bail!("Cannot parse argument {x:?}, we only know the following: {vec:?}")
+                        },
+                    })
+            }
+        }
+    }
+}
+
+create_arg!(
+    enum Arg {
+        Test = "test",
+    }
+);
+
 fn main() -> Result<()> {
     std::env::set_var("RUST_BACKTRACE", "full");
+
+    let arg = Arg::get()
+        .context("Failed to parse arguments")?;
+
+    println!("got arg: {arg:?}");
+
+    return Ok(());
 
     let mut settings = Settings::get()?;
 
