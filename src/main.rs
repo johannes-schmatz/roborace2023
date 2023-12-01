b2089babaae2a65a85747476600b5bed118de922
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-12-01 15:42:56 +0100

    Remove finished todos

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,6 @@ use anyhow::{Context, Result};
 
 use crate::robot::Robot;
 
-// TODO: scp the run.sh over as well!
-//
-//TODO: For actual running do the following:
-// - Measure the black and white values.
-//   Values of about 8 for black and about 94 for white are good. (done!)
-// - Find the matching circle: 78cm, 100cm or 129cm:
-//   - Small one:
-//     We hope that they didn't chose this circle.
-//     We need a relatively large k_p, and a large k_i.
-//     Also is the maximum speed somewhat limited.
-//   - Medium one:
-//     A normal k_p and a normal k_i are sufficient.
-//     We cannot drive with v = 80 because of the problems
-//     with the speed of the right motor. (this one!)
-//   - Large one:
-//     This is the easiest to drive on.
-//     We use our normal values everywhere.
-//     The speed can go up to v = 85 probably.
-// - Ensure that the rotating arm matches their ball position.
-
 fn main() -> Result<()> {
     // We want long stack traces.
     std::env::set_var("RUST_BACKTRACE", "full");
@@ -41,12 +21,6 @@ fn main() -> Result<()> {
             .arg("/usr/share/consolefonts/Lat2-Terminus14.psf.gz")
             .status()?;
     }
-
-    //TODO: consider running via ssh
-    // We run it over ssh on the robot. Not sure if they find that strange or not.
-    //TODO: find a way to sync the robot_settings.toml
-    // We just manually copy over the robot_settings.toml before starting and after
-    // being done.
 
     let bot = Robot::new().context("Failed to create robot")?;
 
